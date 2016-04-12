97e6c5f50e7474af0ce62c653bf278114ec295ac
tvedtorama <tvedtorama@gmail.com>
2016-04-12 13:50:11 +0200

    Circling option list.

--- src/app.js
+++ src/app.js
@@ -6,26 +6,34 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
+var circulator = require('./itemCirculator')
 
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'Start',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
+  subtitle: 'Digital Value Network',
+  body: '',
   subtitleColor: 'indigo', // Named colors
   bodyColor: '#9a0036' // Hex colors
 });
 
 main.show();
 
+// var availability = ['Dont disturb', 'Internally available', 'Externally available'];
+var availability = ["Don't disturb", 'Available', 'Publish location'];
+
+
 var menuFunc = function(e) {
   var items = [{
-        title: 'Pebble.js',
+        title: 'Availability',
         icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
+        subtitle: 'Click to update'
       }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
+        title: 'Internal',
+        subtitle: availability[0]
+      }, {
+        title: 'External',
+        subtitle: availability[0]
       }];
   var menu = new UI.Menu({
     sections: [{
@@ -35,8 +43,10 @@ var menuFunc = function(e) {
   menu.on('select', function(e) {
     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
     console.log('The item is titled "' + e.item.title + '"');
-    items[e.itemIndex].subtitle = items[e.itemIndex].subtitle + '+';
-    menu.items(0, items);
+    if (e.itemIndex > 0) {
+      items[e.itemIndex].subtitle = circulator(items[e.itemIndex].subtitle, availability);
+      menu.items(0, items);      
+    }
     // menu.show();
   });
   menu.show();
